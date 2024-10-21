c3531c5b15ab90caa6babe04528e11dcf6983c64
Munotasir2762 <muntasirpranto2762@gmail.com>
2024-10-21 23:59:19 +0600

    Js loop

---
+++ script.js
@@ -0,0 +1,145 @@
+
+
+// let marks = [56, 61, 90, 30, 58, 99];
+
+// for (let i = 0; i<=5; i++){
+//     console.log (marks[i]);
+// }
+
+// for (let i=1; i<=10; i++){
+//     console.log("hello wordl");
+// }
+
+
+// marks.forEach((element)=>{
+//     console.log(element);
+// })
+
+// let i = 0;
+
+// while(i<=5){
+//     console.log (marks[i]);
+//     i++;
+// }
+
+// let j = 1;
+
+// while(j<=10){
+//     console.log("hello wordl");
+//     j++;
+// }
+
+
+// let k = 0;
+
+// do {
+//     console.log (marks[k]);
+//     k++;  //1,2
+// }
+
+// while(k<=5);
+
+// let l = 11;
+
+// do {
+//     console.log("Hello world");
+//     l++;
+// }
+
+// while(l<=10);
+
+
+let marks = [45, 67, 89, 67, 99, 54, 45, 67, 30, 50, 56, 93, 90, 76, 67, 34, 99, 20, 45, 34, 56, 89, 78, 100, 54];
+
+// let number=33;
+
+// if (number >= 80){
+//     console.log("A+");
+// }
+
+// else if (number >= 70){
+//     console.log("A");
+// }
+
+// else if (number >= 60){
+//     console.log("A-");
+// }
+
+// else if (number >= 50){
+//     console.log("B");
+// }
+// else if (number >= 40){
+//     console.log("C");
+// }
+
+// else if (number>=33){
+//     console.log("D");
+// }
+
+// else{
+//     console.log("F");
+// }
+
+
+// for (let i=0; i<=18; i++){ //0, 1
+
+//     if (marks[i] >= 80){
+//         console.log(marks[i] + "=A+");
+//     }
+    
+//     else if (marks[i] >= 70){
+//         console.log(marks[i] + "=A");
+//     }
+    
+//     else if (marks[i] >= 60){
+//         console.log(marks[i] + "=A-");
+//     }
+    
+//     else if (marks[i] >= 50){
+//         console.log(marks[i] + "=B");
+//     }
+//     else if (marks[i] >= 40){
+//         console.log(marks[i] + "=C");
+//     }
+    
+//     else if (marks[i] >=33){
+//         console.log(marks[i] + "=D");
+//     }
+    
+//     else{
+//         console.log(marks[i] + "=F");
+//     }
+// }
+
+marks.forEach((number)=>{
+    if (number >= 80){
+        console.log(number + "=A+");
+    }
+    
+    else if (number >= 70){
+        console.log(number + "=A");
+    }
+    
+    else if (number >= 60){
+        console.log(number + "=A-");
+    }
+    
+    else if (number >= 50){
+        console.log(number + "=B");
+    }
+    else if (number >= 40){
+        console.log(number + "=C");
+    }
+    
+    else if (number >=33){
+        console.log(number + "=D");
+    }
+    
+    else{
+        console.log(number + "=F");
+    }
+})
+
+
+
+
